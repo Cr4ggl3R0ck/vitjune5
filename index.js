772c9dc3b8ea6845117a81c3b802625a13bad224
nrichardson14 <nicholerichardson4316@gmail.com>
2020-06-05 16:21:54 +0900

    added html js css

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+let changeColorButton = document.querySelector('#changeColor') 
+let colorDiv = document.querySelector('#colorDiv')
+
+changeColorButton.addEventListener('click', function(){
+    colorDiv.style.background = 'green'
+})
+
+
+})
